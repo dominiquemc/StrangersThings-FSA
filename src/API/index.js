12da0d094b2957fd4b874e45fb221b3f9812661d
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-05 15:56:55 -0500

    started process of adding API to fetch posts

--- src/API/index.js
+++ src/API/index.js
@@ -2,3 +2,32 @@
 
 const COHORT_NAME = "2302-ACC-CT-WEB-PT-A";
 const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}/`;
+
+// GET POSTS
+
+export const getPosts = async () => {
+  try {
+    const post = {
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${TOKEN_STRING_HERE}`,
+      },
+    };
+    const response = await fetch(`${BASE_URL}posts`);
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//   try {
+//     const response = await fetch(`${BASE_URL}posts`);
+//     const result = await response.json();
+//     console.log(result);
+//     return result;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };

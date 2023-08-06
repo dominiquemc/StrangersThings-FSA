39860c70a73a970ad8dbd01b55757c69e86593eb
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-06 01:07:21 -0500

    Testing initial component for UserPosts to check if data renders on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import "./App.css";
+import UserPosts from "./components/Posts";
 
-function App() {}
-
+function App() {
+  return (
+    <div>
+      <h1>User Posts</h1>
+      <UserPosts />
+    </div>
+  );
+}
 export default App;

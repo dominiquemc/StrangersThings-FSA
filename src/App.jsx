ec1299016d5974ea593a76ca1ad323990e852298
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-07 00:38:49 -0500

    imported Link, Routes, Route, and added navigation + initial route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import "./App.css";
 import UserPosts from "./components/Posts";
+import Home from "./components/Home";
+import { Link, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>User Posts</h1>
-      <UserPosts />
-    </div>
+    <>
+      <nav>
+        <h1>Strangers Things</h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<UserPosts />} />
+      </Routes>
+    </>
   );
 }
 export default App;

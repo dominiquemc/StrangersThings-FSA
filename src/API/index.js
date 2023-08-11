8f066ac62e4e41b7d17b8bbe2f73c40b7dffe523
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-10 20:13:42 -0500

    added registerUser function

--- src/API/index.js
+++ src/API/index.js
@@ -22,3 +22,28 @@ export const getPosts = async () => {
     console.log(err);
   }
 };
+
+// REGISTER ACCOUNT
+
+export const registerUser = async (username, password) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: username,
+          password: password,
+        },
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};

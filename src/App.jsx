0130ecff87dddcba3b88be4338f7b623c17b002d
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-10 20:15:12 -0500

    imported Register, Login components, added link and route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import UserPosts from "./components/Posts";
 import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
 import { Link, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -15,11 +17,16 @@ function App() {
           <li>
             <Link to="/posts">Posts</Link>
           </li>
+          <li>
+            <Link to="/account/login">Login</Link>
+          </li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/posts" element={<UserPosts />} />
+        <Route path="/account/register" element={<Register />} />
+        <Route path="/account/login" element={<Login />} />
       </Routes>
     </>
   );

8c760cdba53bc2d155d2cf68067b661b97d6e472
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-17 18:46:10 -0500

    added login to account API call

--- src/API/index.js
+++ src/API/index.js
@@ -47,3 +47,28 @@ export const registerUser = async (username, password) => {
     console.error(err);
   }
 };
+
+// LOGIN TO ACCOUNT
+
+export const loginUser = async (username, password) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: username,
+          password: password,
+        },
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};

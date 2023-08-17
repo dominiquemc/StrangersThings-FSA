4f4a53b93e0badc7a5d1b98f3021475c988a0fda
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-17 18:47:47 -0500

    added shopping logo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <div className="home">
+      <img
+        src="src/assets/shopping.svg"
+        className="shoppinglogo"
+        alt="Shopping Logo"
+      />
       <h1>Welcome to Strangers Things</h1>
+      {/* <p>
+        Join our growing community for high quality items at affordable prices!
+      </p> */}
     </div>
   );
 }

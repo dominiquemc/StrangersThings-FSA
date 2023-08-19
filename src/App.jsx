9c5cd8953e7bd4ae434ffe30aabf02177f363d0e
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-18 19:13:34 -0500

    removed nav and unneeded imports from file, added AuthProvider + maintained routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,25 @@
 import "./App.css";
-import UserPosts from "./components/Posts";
+import Nav from "./components/Navbar";
 import Home from "./components/Home";
+import UserPosts from "./components/Posts";
 import Register from "./components/Register";
 import Login from "./components/Login";
-import { Link, Routes, Route } from "react-router-dom";
-import { registerUser } from "./API";
-import { useEffect } from "react";
+import Logout from "./components/Logout";
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./components/Auth";
 
 function App() {
-  // const [username, setUsername] = useState("")
-
   return (
-    <>
-      <nav>
-        <h1>Strangers Things</h1>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/posts">Posts</Link>
-          </li>
-          <li>
-            <Link to="/account/login">Login</Link>
-          </li>
-
-          {registerUser && (
-            <li>
-              <Link to="/account/login">Profile</Link>
-            </li>
-          )}
-
-          {registerUser && (
-            <li>
-              <Link to="/account/login">Logout</Link>
-            </li>
-          )}
-        </ul>
-      </nav>
+    <AuthProvider>
+      <Nav />;
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/posts" element={<UserPosts />} />
         <Route path="/account/register" element={<Register />} />
         <Route path="/account/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
       </Routes>
-    </>
+    </AuthProvider>
   );
 }
 export default App;
-
-// changing nav upon login
-// success/error messsages

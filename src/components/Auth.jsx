c03ac1c519c4efeda9a5388afccc5a98c5d0489f
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-22 19:41:08 -0500

    added user, setUser state for auth changes

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,19 +3,26 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(
-    localStorage.getItem("username") && localStorage.getItem("password")
-  );
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
+  const [user, setUser] = useState(null);
 
-  const handleAuthChange = (loggedIn) => {
+  const handleAuthChange = (loggedIn, userData = null) => {
     setIsLoggedIn(loggedIn);
+    if (userData) {
+      setUser(userData);
+    }
   };
+
+  // Log the user data before calling handleAuthChange
+  console.log("User data before setting:", user);
+
   return (
-    <AuthContext.Provider value={{ isLoggedIn, handleAuthChange }}>
+    <AuthContext.Provider value={{ isLoggedIn, user, handleAuthChange }}>
       {children}
     </AuthContext.Provider>
   );
 }
+
 export function useAuth() {
   return useContext(AuthContext);
 }

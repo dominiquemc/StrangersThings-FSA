364190498cfbe75983a1a4643d0a8aca254b9ae5
Ana Alicia Louis <anaaliciareed@gmail.com>
2023-08-23 16:03:15 -0500

    adding something new

--- src/API/index.js
+++ src/API/index.js
@@ -72,3 +72,29 @@ export const loginUser = async (username, password) => {
     console.error(err);
   }
 };
+
+
+
+// Messages
+
+export const postMessage = async () => {
+  try {
+    const response = await fetch('${BASE_URL}/posts/5e8929ddd439160017553e06/messages', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ${TOKEN_STRING_HERE}'
+      },
+      body: JSON.stringify({
+        message: {
+          content: "Do you still have this? Would you take $10 less?"
+        }
+      })
+    });
+    const result = await response.json();
+    console.log(result);
+    return result
+  } catch (err) {
+    console.error(err);
+  }
+}

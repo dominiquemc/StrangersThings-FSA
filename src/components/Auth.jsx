5d6d74119ba275adda9403abc99b092097589889
Dominique C <85527930+dominiquemc@users.noreply.github.com>
2023-08-23 22:31:54 -0500

    Fixed Auth.jsx code

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,7 +3,6 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-
   const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
   const [user, setUser] = useState(null);
 
@@ -14,34 +13,19 @@ export function AuthProvider({ children }) {
       setUser(userData);
     }
   };
-  // Log the user data before calling handleAuthChange
+
   console.log("User data before setting:", user);
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, user, handleAuthChange }}>
-
-  const [isLoggedIn, setIsLoggedIn] = useState(
-    localStorage.getItem("username") && localStorage.getItem("password")
-  );
-
-  const handleAuthChange = (loggedIn) => {
-    setIsLoggedIn(loggedIn);
-  };
-  return (
-    <AuthContext.Provider value={{ isLoggedIn, handleAuthChange }}>
       {children}
     </AuthContext.Provider>
   );
 }
 
-
 export function useAuth() {
   return useContext(AuthContext);
 }
-
-
-
-
 
 
 

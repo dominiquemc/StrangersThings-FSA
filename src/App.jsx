651c32214d25347235865c9c0c0f7cc0834829d6
Dominique C <85527930+dominiquemc@users.noreply.github.com>
2023-08-23 22:25:35 -0500

    Fixed App.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./components/Home";
 import UserPosts from "./components/Posts";
 import Register from "./components/Register";
 import Login from "./components/Login";
-
 import UserProfile from "./components/Profile";
 import Logout from "./components/Logout";
 import MakePost from "./components/MakeAPost";
@@ -18,31 +17,14 @@ function App() {
 
   return (
     <AuthProvider>
-
-import { Routes, Route } from "react-router-dom";
-import MakePost from "./components/MakePost";
-import MyPosts from "./components/MyPosts";
-import { AuthProvider } from "./components/Auth";
-import Nav from "./components/Nav";
-import Logout from "./components/Logout";
-import { useState } from "react";
-
-// import { useEffect } from "react";
-
-function App() {
-// const [posts, setPosts] = useState([]);
-
-  return (
-<AuthProvider>
-
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/posts" element={<UserPosts />} />
         <Route path="/account/register" element={<Register />} />
         <Route path="/account/login" element={<Login />} />
-
-        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/profile" element={<MyPosts />} />
+        <Route path="/makepost" element={<MakePost />} />
         <Route path="/logout" element={<Logout />} />
         <Route
           path="/makepost"
@@ -50,18 +32,8 @@ function App() {
         />
       </Routes>
       <ToastContainer />
-
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/makepost" element={<MakePost />} />
-        <Route path="/profile" element={<MyPosts />}></Route>
-      </Routes>
-
     </AuthProvider>
   );
 }
 export default App;
-
-
-// changing nav upon login
-// success/error messages
 
